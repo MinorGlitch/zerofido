82ecb3da8f5aa4ca41b21d3c184996f8fe3f6339
Alex Stoyanov <28191524+MinorGlitch@users.noreply.github.com>
2026-04-29 21:16:11 +0300

    chore: add attestation source license headers

--- src/attestation/local.h
+++ src/attestation/local.h
@@ -1,3 +1,12 @@
+/*
+ * ZeroFIDO
+ * Copyright (C) 2026 Alex Stoyanov
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 or later.
+ */
+
 #pragma once
 
 #include <stdbool.h>
